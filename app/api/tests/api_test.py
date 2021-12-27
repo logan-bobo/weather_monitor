a68ff0e1901ad912c42de92d85e27dea8bc506d4
Logan-bobo <logan.bobofed@gmail.com>
2021-12-27 16:12:00 +0000

    updating tests

--- app/api/tests/api_test.py
+++ app/api/tests/api_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pytest
 
 from src.main import health, getweather
@@ -7,7 +9,7 @@ def test_health():
     health_output = health()
     assert health_output == {"Health": "Green"}
 
-# Test for our loactions
+# Test for our locations
 def test_location():
     weather = getweather(1234)
     assert weather == {"location": 1234}
