938d875adf8fa0690a8d416e3b39cebcff88a10c
Logan-bobo <logan.bobofed@gmail.com>
2022-01-03 12:43:29 +0000

    debugging CI

--- app/api/src/main.py
+++ app/api/src/main.py
@@ -22,6 +22,7 @@ def getweather( latitude: float, longitude: float ):
     location = engine.reverse(f"{latitude}, {longitude}")
     address = location.address.split(",")
     city = address[4]
+    print(city)
 
     # Request to open weather with out city name and unity to grab back all weather conditions for that city
     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}")
