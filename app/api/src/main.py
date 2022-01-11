396578b8e79006a07cbc6fe9f6b4d98eb9a0beb9
Logan-bobo <logan.bobofed@gmail.com>
2022-01-11 21:10:39 +0000

    linting

--- app/api/src/main.py
+++ app/api/src/main.py
@@ -5,24 +5,26 @@ Instantiate FastAPI along with functions to convert latitude and longitude to te
 """
 
 import os
+from functools import lru_cache
 import requests
 
 from geopy.geocoders import Nominatim
 from fastapi import FastAPI
 
 # Create an instance of the FastAPI
-app = FastAPI()
+APP = FastAPI()
 # Set our main api key
-api_key = os.environ.get('API_KEY')
+API_KEY = os.environ.get('API_KEY')
 UNITS = "metric"
 # Create an instance of our engine to convert longitude and latitude to an address
-engine = Nominatim(user_agent="google")
+ENGINE = Nominatim(user_agent="google")
 
-def get_address( latitude: float, longitude: float):
+@lru_cache(maxsize=None)
+def get_address(latitude: float, longitude: float):
     """ Return a city name based on latitude and longitude"""
 
     # Search our location based on longitude and latitude
-    location = engine.reverse(f"{latitude}, {longitude}")
+    location = ENGINE.reverse(f"{latitude}, {longitude}")
 
     # Get the raw output from out location lookup and select the keys that give us the city name
     address = location.raw["address"]["city"]
@@ -34,12 +36,11 @@ def get_temperature(city: str):
     # Request to open weather with our city name and unit to grab back all weather
     # conditions for that city
     response = requests.get("https://api.openweathermap.org/data/2.5/weather",
-        params = {
-            'q' : city,
-            'appid' : api_key,
-            'units' : UNITS,
-        }
-    )
+                            params={
+                                'q' : city,
+                                'appid' : API_KEY,
+                                'units' : UNITS,
+                            })
 
     # Set the response JSON to be the json output of the request
     response_json = response.json()
@@ -50,8 +51,8 @@ def get_temperature(city: str):
     return temperature
 
 # Send user location to open weather and return the weather based on location
-@app.get("/location/{latitude}/{longitude}")
-def getweather( latitude: float, longitude: float ):
+@APP.get("/location/{latitude}/{longitude}")
+def getweather(latitude: float, longitude: float):
     """ Returns a temperature based on a latitude and longitude """
     city = get_address(latitude, longitude)
     temperature = get_temperature(city)
@@ -59,7 +60,7 @@ def getweather( latitude: float, longitude: float ):
     return {"temperature" : temperature}
 
 # API health endpoint
-@app.get("/health")
+@APP.get("/health")
 def health():
     """ Returns the health of the API"""
     return {"Health": "Green"}
